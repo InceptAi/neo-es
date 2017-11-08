3652393bc142f77106603192d6e7312eaedfee48
Arunesh Mishra <aruneshm@gmail.com>
2017-11-08 15:28:25 -0800

    first commit

---
+++ src/main/java/com/inceptai/neo/es/App.java
@@ -0,0 +1,13 @@
+package com.inceptai.neo.es;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
